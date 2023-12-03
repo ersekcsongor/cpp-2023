7ed8f8bedce50e3d4732e14193112a9f0420eea2
ersekcsongor <ersek.csongor@student.ms.sapientia.ro>
2023-12-03 13:41:12 +0200

    labor10

--- lab10/main_10.cpp
+++ lab10/main_10.cpp
@@ -5,41 +5,7 @@
 #include "Ceg.h"
 
 int main() {
-   /* Szemely szemely1("John", "Doe", 1990);
-    szemely1.print(cout);
-    cout << endl;
-    cout <<  szemely1 << endl;
-    Alkalmazott alkalmazott1("Kis","Csaba",1993,"programozo");
-    Alkalmazott alkalmazott2("Madarasz","Adam",1987,"programozo");
-    alkalmazott1.print(cout);
-    cout << endl;
-    cout << alkalmazott1 << endl;
-    Manager manager1("Nagy","Pal",1995,"manager");
-    manager1.addAlkalmazott(&alkalmazott1);
-    manager1.addAlkalmazott(&alkalmazott2);
-//    manager1.print(cout);
-//    cout << endl;
-    cout << manager1;
-    cout << endl;
-    manager1.deleteAlkalmazott(1);
-    cout << manager1;
-    cout << endl;
-    cout << "Beosztottak szama: " << manager1.beosztottakSzama();
-    cout << endl;
-    cout << alkalmazott1.getID();
-    cout << endl;
-    cout << alkalmazott2.getID();
-    cout << endl;
-    Ceg ceg1("3ss");
-    Manager manager2("Dezso","Elemer",1985,"manager");
-    manager2.addAlkalmazott(&alkalmazott1);
-    ceg1.hireEmployees(&manager2);
-    ceg1.hireEmployees(&manager1);
-    ceg1.print(cout);
-    ceg1.fireEmployees(4);
-    ceg1.print(cout);
-    manager1.deleteAlkalmazott(2);
-    ceg1.print(cout);*/
+
     Ceg ceg1("Lynx Solutions");
     Alkalmazott alkalmazott1("Kis", "Csaba", 1993, "programozo");
     Alkalmazott alkalmazott2("Madarasz", "Adam", 1987, "programozo");
