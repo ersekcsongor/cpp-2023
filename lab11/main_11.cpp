11beb66b05481d1028981b149fc8cce0b084a091
ersekcsongor <ersek.csongor@student.ms.sapientia.ro>
2023-12-09 20:33:11 +0200

    lab11

--- lab11/main_11.cpp
+++ lab11/main_11.cpp
@@ -7,46 +7,6 @@
 
 int main() {
     std::cout << "Hello, Lab_11" << std::endl;
-   /* Student student1(1, "Nagy", "Elemer");
-    cout << student1.getFirstName() << endl;
-    cout << student1.getLastName() << endl;
-    student1.addGrade("math", 5);
-    student1.addGrade("hungarian", 5);
-    student1.addGrade("romanian", 5);
-    cout << student1.getGrade("math") << endl;
-    cout << student1.getGrade("hungarian") << endl;
-    cout << student1.getGrade("romanian") << endl;
-    cout << "******" << endl;
-    try {
-        cout << student1.getGrade("foldrajz") << endl;
-    } catch (out_of_range &e) {
-        cout << "Nem letezo tantargy\n";
-    }
-
-    map<string, double> jegyek = student1.getGrades();
-    for (auto &item: jegyek) {
-        cout << item.first << " " << item.second << endl;
-    }
-    student1.computeAverage();
-    cout << "average: " << student1.getAverage() << endl;
-    cout << student1 << endl;
-    GraduationDao graduationDao1(2021);
-    graduationDao1.enrollStudents("names.txt");
-    graduationDao1.saveGradesForSubject("math", "math.txt");
-    graduationDao1.saveGradesForSubject("hungarian", "hungarian.txt");
-    graduationDao1.saveGradesForSubject("romanian", "romanian.txt");
-    graduationDao1.computeAverage();
-    cout << "Enrolled students number: " << graduationDao1.numEnrolled() << endl;
-    cout << "Passed students number: " << graduationDao1.numPassed() << endl;
-//    graduationDao1.printStudents(cout);
-    try {
-        cout << graduationDao1.findById(5435) << endl;
-    } catch (out_of_range &e) {
-        cout << "Non existent id!" << endl;
-    }
-    cout << graduationDao1.getAverageBySubject("math") << endl;
-    cout << graduationDao1.getAverageBySubject("romanian") << endl;
-    cout << graduationDao1.getAverageBySubject("hungarian") << endl;*/
    vector <string> subjects = {"hungarian","romanian","math"};
    int numSubjects = subjects.size();
    GraduationDao dao(2023);
