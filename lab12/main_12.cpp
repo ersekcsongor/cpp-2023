9cfb0c9c092f5ea5e25fd1ea8e940b68c05fb2b9
ersekcsongor <ersek.csongor@student.ms.sapientia.ro>
2023-12-16 22:30:28 +0200

    labor12

--- lab12/main_12.cpp
+++ lab12/main_12.cpp
@@ -9,25 +9,24 @@ int main() {
     cout  << settlement1 << endl;
     multimap<string,Settlement>megyek;
     SettlementStatisticsImpl settlementStatistics1(megyek);
-    cout << "Telepulesek:" << settlementStatistics1.numSettlements() << endl;
-    cout << "Megyek:" << settlementStatistics1.numCounties() << endl;
     multimap <string, Settlement> settlementData;
     SettlementStatisticsImpl setlStat1(settlementData);
     setlStat1.readSettlements("telepulesek.csv");
 //    cout << setlStat1;
     cout <<  "Number of settlements: " << setlStat1.numSettlements() << endl;
     cout <<  "Number of counties:0"<< setlStat1.numCounties() << endl;
-    cout <<  "Number of settlements in given county: " << setlStat1.numSettlementsByCounty("AB") << endl;
+    cout <<  "Number of settlements in Alba county: " << setlStat1.numSettlementsByCounty("AB") << endl;
     vector<Settlement>settlements;
     cout <<  "Settlement by Name and County: " << setlStat1.findSettlementsByNameAndCounty("Sanmiclaus","AB") << endl;
     settlements = setlStat1.findSettlementsByCounty("AB");
     vector<Settlement>settlements2;
+    cout << "Finding settlements with the same name from different counties: " << endl;
     settlements2 = setlStat1.findSettlementsByName("Sanmiclaus");
     for(auto &e : settlements2)
     {
         cout << e << endl;
     }
-    cout <<  setlStat1.maxPopulationDensity() <<  endl;
-    cout <<  setlStat1.minPopulationDensity() <<  endl;
+    cout << "County with the highest poulation: " <<  setlStat1.maxPopulationDensity() <<  endl;
+    cout <<  "County with the lowest poulation: " << setlStat1.minPopulationDensity() <<  endl;
     return 0;
 }
